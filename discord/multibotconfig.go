6fbaba65b83d5996b0209348d9b00ef902b6037d
liuliuDog <167843603+liuliuDog@users.noreply.github.com>
2024-06-07 16:47:46 +0800

    Update multibotconfig.go

--- discord/multibotconfig.go
+++ discord/multibotconfig.go
@@ -9,6 +9,11 @@ import (
 var MultiBotConfig = os.Getenv("MULTI_BOT_CONFIG")
 
 func loadMultiBotConfig() []byte {
-    common.SysLog(fmt.Sprintf("读取配置 MultiBotConfig: %+v", MultiBotConfig))
-    return []byte(MultiBotConfig)
+    
+    if MultiBotConfig != "" {
+        common.SysLog(fmt.Sprintf("读取配置 MultiBotConfig: %+v", MultiBotConfig))
+        return []byte(MultiBotConfig)
+    }
+    common.FatalLog("环境变量 MULTI_BOT_CONFIG 未设置")
+    return
 }
